239de905de98259bbf9535558074afc1799b7685
Byron Collins <byronical@gmail.com>
2024-03-18 18:27:29 +0900

    add some debugging

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,4 +1,5 @@
 const { MailtrapClient } = require("mailtrap");
+const https = require("https");
 
 const TOKEN = process.env.TOKEN;
 const ENDPOINT = "https://send.api.mailtrap.io/";
@@ -21,27 +22,46 @@ exports.handler = async function (event, context, callback) {
   // Parse the JSON text received.
   const { name, email, message } = JSON.parse(event.body);
 
+  const data = JSON.stringify(JSON.parse(event.body));
+
   console.log(`Recieved a submission: ${JSON.stringify(JSON.parse(event.body))}`);
 
-  const headers = new Headers()
-  headers.append("Content-Type", "application/json")
-
   const options = {
+    host: "eog1wjkob2icej2.m.pipedream.net",
+    port: 443,
+    path: "/",
     method: "POST",
-    headers,
     mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+    },    
     body: JSON.stringify(event.body),
   }
   
-  fetch("https://eog1wjkob2icej2.m.pipedream.net", options)
+  const req = https.request(options, (res) => {
+      console.log('statusCode:', res.statusCode);
+      console.log('headers:', res.headers);
+    
+      res.on('data', (d) => {
+        process.stdout.write(d);
+      });
+    });
 
+  req.write(data);
+
+  req.on('error', (e) => {
+    console.error(e);
+  });
+
+  req.end();
 
   // Build an HTML string to represent the body of the email to be sent.
   const html = `
   <div style="margin: 20px auto;">Name: ${name}</div>
   <div style="margin: 20px auto;">Email: ${email}</div>
   <div style="margin: 20px auto;">Message: ${message}</div>
-    `
+    `;
 
 client
   .send({
